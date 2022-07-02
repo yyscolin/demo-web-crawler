acd769702f9896b86d71d9140426a245595d0525
yyscolin <48078302+yyscolin@users.noreply.github.com>
2022-07-02 21:06:40 +1000

    Enhanced WebCrawler's error handling capabilities

--- web-crawler.js
+++ web-crawler.js
@@ -1,50 +1,74 @@
 const getWebDriver = require('./web-driver')
-const ERRORS = require(`./crawl-errors`)
 
 
-async function getSearchResults(webProfile, searchQuery) {
+async function findUrlsToCrawl(webProfile, searchQuery) {
   const retries_limit = 3
+  let errorCode = 2
+
   for (let i = 0; i < retries_limit; i++) {
+    const {hostname, getLandingUrl, getSearchResults} = webProfile
+    let webDriver
+
     try {
-      const {hostname, getLandingUrl, getSearchResults} = webProfile
       const landingUrl = await getLandingUrl(hostname, searchQuery)
-      const webDriver = await getWebDriver(landingUrl)
+      webDriver = await getWebDriver(landingUrl)
+    } catch(error) {
+      console.log(error)
+      return [2, []]
+    }
 
-      try {
-        const isCloudflared = await webProfile.isCloudflared(webDriver)
-        if (isCloudflared) throw ERRORS[`-1`]
-      } catch(error) {
-        console.error(error)
-        if (error.code != -1) {
-          console.error(error)
-          error.code = 3
-        }
-        throw error
+    try {
+      const isCloudflared = await webProfile.isCloudflared(webDriver)
+      if (isCloudflared) {
+        await webDriver.quit()
+        return [-1, []]
       }
+    } catch(error) {
+      console.log(error)
+      errorCode = 3
+      await webDriver.quit()
+      continue
+    }
 
-      try {
-        const hasResults = await webProfile.hasResults(webDriver, searchQuery)
-        if (!hasResults) throw ERRORS[`-2`]
-      } catch(error) {
-        console.error(error)
-        if (error.code != -2) {
-          console.error(error)
-          error.code = 4
-        }
-        throw error
+    try {
+      const hasResults = await webProfile.hasResults(webDriver, searchQuery)
+      if (!hasResults) {
+        await webDriver.quit()
+        return [-2, []]
       }
+    } catch(error) {
+      console.log(error)
+      errorCode = 4
+      await webDriver.quit()
+      continue
+    }
 
+    try {
       const urlsToCrawl = await getSearchResults(webDriver, searchQuery)
-     
+
+      if (!Array.isArray(urlsToCrawl)) {
+        console.log(`Error: getSearchResults() must return an array`)
+        await webDriver.quit()
+        return [5, []]
+      }
+
+      if (!urlsToCrawl.length) {
+        await webDriver.quit()
+        return [-3, []]
+      }
+
       if (!urlsToCrawl.includes(webDriver.currentUrl))
         await webDriver.quit()
 
-      return urlsToCrawl
+      return [0, urlsToCrawl]
     } catch(error) {
-      if (error.code < 0) throw error
+      console.log(error)
+      errorCode = 5
+      await webDriver.quit()
     }
   }
-  throw ERRORS[`2`]
+
+  return [errorCode, []]
 }
 
 async function handleSearchQuery(searchQuery, webProfile) {
@@ -56,7 +80,13 @@ async function handleSearchQuery(searchQuery, webProfile) {
     }
 
     try {
-      const urlsToCrawl = await getSearchResults(webProfile, searchQuery)
+      const searchResults = await findUrlsToCrawl(webProfile, searchQuery)
+      const [searchCode, urlsToCrawl] = searchResults
+      if (searchCode) {
+        crawlResult.code = searchCode
+        return crawlResult
+      }
+
       crawlResult.payload = await Promise.all(
         urlsToCrawl.map(async urlToCrawl => {
           const webDriver = await getWebDriver(urlToCrawl)
